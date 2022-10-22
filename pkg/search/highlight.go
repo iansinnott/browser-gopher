62ba7b8fba23a309f89046459f20a37d7ad453a4
Ian Sinnott <ian@iansinnott.com>
2022-10-22 17:56:25 +0800

    fix highlighting bug

--- pkg/search/highlight.go
+++ pkg/search/highlight.go
@@ -1,6 +1,7 @@
 package search
 
 import (
+	"fmt"
 	"strings"
 
 	bs "github.com/blevesearch/bleve/v2/search"
@@ -17,6 +18,15 @@ func HighlightAll(locations bs.TermLocationMap, text string, render HighlightMat
 			if lastEnd > loc.Start {
 				// locations out of order? if you slice something like [12:3] it will fail. first must be less than second
 				continue
+			}
+
+			// @note This seems to be an error. In my limited testing it appears to
+			// come up when text does not contain any of the terms in the location
+			// map. Why would that be the case though?
+			if len(text) < int(loc.Start) || len(text) < int(loc.End) {
+				// @todo there should be a debug logger here
+				fmt.Printf("WARNING: location out of bounds. text: %s, loc: %+v, mapping: %+v", text, loc, locations)
+				return text
 			}
 
 			xs = append(xs, text[lastEnd:loc.Start])
