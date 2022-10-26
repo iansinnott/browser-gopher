03b7a26f43b522d9bc43ad11784317223a11c292
Ian Sinnott <ian@iansinnott.com>
2022-10-26 22:33:21 +0800

    add scraper tests

---
+++ pkg/fulltext/scraper_test.go
@@ -0,0 +1,46 @@
+package fulltext_test
+
+import (
+	"testing"
+
+	"github.com/iansinnott/browser-gopher/pkg/fulltext"
+	"github.com/stretchr/testify/require"
+)
+
+// These tests are decidedly not good, in that they depend on the outside world.
+// They could fail randomly due to network conditions, DNS issuse, updates in
+// the resolved web apps, etc.
+func TestScrapeUrls(t *testing.T) {
+	table := []struct {
+		name string
+		url  string
+	}{
+		{
+			name: "scrape a single website",
+			url:  "https://example.com",
+		},
+		{
+			name: "handle redirects",
+			url:  "https://iansinnott.com", // redirects to https://www.iansinnott.com
+		},
+	}
+
+	scraper := fulltext.NewScraper()
+
+	for _, tt := range table {
+		t.Run(tt.name, func(t *testing.T) {
+			xm, err := scraper.ScrapeUrls([]string{tt.url})
+			require.Nil(t, err)
+			require.NotEmpty(t, xm[tt.url])
+			body := xm[tt.url].Body
+			require.NotEmpty(t, body)
+		})
+	}
+
+	// t.Run("scrape multiple websites", func(t *testing.T) {
+	// 	xm, err := scraper.ScrapeUrls([]string{"https://example.com", "https://iansinnott.com"})
+	// 	require.Nil(t, err)
+	// 	require.NotEmpty(t, xm["https://example.com"].Body)
+	// 	require.NotEmpty(t, xm["https://iansinnott.com"].Body)
+	// })
+}
